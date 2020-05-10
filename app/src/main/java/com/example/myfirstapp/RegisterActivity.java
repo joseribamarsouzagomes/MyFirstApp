7efb6900f5ef177096351277f1d1a5591150cf8f
Jose Ribamar <jrsouzagomes@gmail.com>
2020-05-09 22:10:48 -0400

    add image for readme

--- app/src/main/java/com/example/myfirstapp/RegisterActivity.java
+++ app/src/main/java/com/example/myfirstapp/RegisterActivity.java
@@ -10,5 +10,6 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 }
